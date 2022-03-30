08f9c71cb91f50ad02a16bdb1ce34a6afa4ff365
Mamont <muhina.katerina@yandex.ru>
2022-03-30 19:04:51 +0300

    Продолжение 4 проекта -> 5 проектная работа

--- README.md
+++ README.md
@@ -8,8 +8,10 @@
 
 ## Функциональность сайта
 
-1. Можно редактирваться Имя профиля и род занятий.
-2. Можно ставить и убирать лайк под фото.
+1. Можно редактирваться имя профиля и род занятий.
+2. Можно добавлять собственные фотокарточки.
+3. Можно ставить и убирать лайк под фотокарточкой.
+4. Можно удалить фотокарточку.
 
 ## Стек технологий:
 - HTML5;
@@ -26,9 +28,11 @@
 
 ## Бриф Проектной работы в Figma:
 - [Бриф 4](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=28212%3A326).
+- [Бриф 5](https://www.figma.com/file/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5?node-id=0%3A1).
 
 ## Чеклист Проектной работы:
 - [Чеклист 4](https://code.s3.yandex.net/web-developer/checklists-pdf/new-program/checklist-4.pdf).
+- [Чеклист 5](https://code.s3.yandex.net/web-developer/checklists-pdf/new-program/checklist-5.pdf).
 
 ## Ссылка на сайт:
 - [Сайт](https://ezdovoymamont.github.io/mesto/)
--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,141 @@
-const popupElement = document.querySelector('.popup');
 const editProfile = document.querySelector('.edit-profile');
-const closeButton = popupElement.querySelector('.popup__close-button');
-const saveButton = document.querySelector('.popup__save-button');
 const userName = document.querySelector('.profile__name');
 const userJob = document.querySelector('.profile__job');
-const likeButtons = document.querySelectorAll('.element__like');
+const popupProfile = document.querySelector('.popup_type_profile')
+const popupProfileNameInput = popupProfile.querySelector('.popup__input_type_name');
+const popupProfileJobInput = popupProfile.querySelector('.popup__input_type_job');
+const closeButtonProfile = popupProfile.querySelector('.popup__close-button');
+const profileForm = popupProfile.querySelector('.popup__form_type_profile');
 
-const nameInput = document.querySelector('.popup__input_type_name');
-const jobInput = document.querySelector('.popup__input_type_job');
+const addButton = document.querySelector('.add-button');
+const popupAddPhoto = document.querySelector('.popup_type_photo')
+const popupProfileTitleInput = popupAddPhoto.querySelector('.popup__input_type_title');
+const popupProfileLinkInput = popupAddPhoto.querySelector('.popup__input_type_link');
+const closeButtonPhoto = popupAddPhoto.querySelector('.popup__close-button');
+const photoForm = popupAddPhoto.querySelector('.popup__form_type_photo');
 
-function openPopup() {
-	popupElement.classList.add('popup_open');
+const popupView = document.querySelector('.popup__view-photo');
+const popupButtonView = popupView.querySelector('.popup__close-button');
+const popupViewPhoto = popupView.querySelector('.popup__photo');
+const popupViewDesc = popupView.querySelector('.popup__description');
 
-	nameInput.value = userName.textContent;
-	jobInput.value = userJob.textContent;
+const elementList = document.querySelector('.elements__list');
+
+const initialCards = [
+	{
+	  name: 'Архыз',
+	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+	},
+	{
+	  name: 'Челябинская область',
+	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+	},
+	{
+	  name: 'Иваново',
+	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+	},
+	{
+	  name: 'Камчатка',
+	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+	},
+	{
+	  name: 'Холмогорский район',
+	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+	},
+	{
+	  name: 'Байкал',
+	  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+	}
+	];
+
+	// Cоздаем новые фотокарточки 
+function createCard(cardPhoto, cardName) {
+	const cardTemplate = document.querySelector('#card-template').content;
+	const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+	const cardElementPhoto = cardElement.querySelector('.element__photo');
+  
+	cardElementPhoto.src = cardPhoto;
+	cardElementPhoto.alt = cardName;
+	cardElement.querySelector('.element__title').textContent = cardName;
+  
+	// Лайк фотокарточки 
+	cardElement.querySelector('.element__like').addEventListener('click', function(evt) {
+		evt.target.classList.toggle('element__like_active');
+	})
+  
+	// Удаление фотокарточки 
+	cardElement.querySelector('.element__trash').addEventListener('click', function(evt) {
+		evt.target.closest('.element').remove();
+	})
+  
+	// Открытие фотокарточки в popup окне
+	cardElement.querySelector('.element__photo').addEventListener('click', function() {
+		popupViewPhoto.src = cardPhoto;
+		popupViewPhoto.alt = cardName;
+	  	popupViewDesc.textContent = cardName;
+		  
+		openPopup(popupView);
+	})
+
+	return cardElement;
 }
 
-function closePopup() {
-	popupElement.classList.remove('popup_open');
+// Функция перебора массива с фотокарточками
+
+function renderInitialCards(initialCards) {
+	initialCards.forEach((item) => {
+		elementList.append(createCard(item.link, item.name));
+	})
 }
 
-editProfile.addEventListener('click', openPopup)
-closeButton.addEventListener('click', closePopup)
+renderInitialCards(initialCards);
 
-let formElement = document.querySelector('.popup__form'); 
-
-
-function formSubmitHandler (evt) {
-	evt.preventDefault(); 
-
-	userName.textContent = nameInput.value;
-	userJob.textContent = jobInput.value;
-
-	closePopup();
+// Открытие popup
+function openPopup(popup) {
+	popup.classList.add('popup_open');
 }
 
-formElement.addEventListener('submit', formSubmitHandler); 
+// Закрытие popup
+function closePopup(popup) {
+	popup.classList.remove('popup_open');
+}
+
+// Открытие popup по клику на редактировать профиль или добавить фотокарточку
+editProfile.addEventListener('click', function() {
+	popupProfileNameInput.value = userName.textContent;
+	popupProfileJobInput.value = userJob.textContent;
+
+	openPopup(popupProfile);
+})
+
+addButton.addEventListener('click', () => openPopup(popupAddPhoto));
+
+// Закрытие popup по клику
+closeButtonProfile.addEventListener('click', () => closePopup(popupProfile));
+closeButtonPhoto.addEventListener('click', () => closePopup(popupAddPhoto));
+popupButtonView.addEventListener('click', () => closePopup(popupView));
+
+// Функция формы для изменения профиля
+function formSubmitHandler(evt) {
+	evt.preventDefault();
+  
+	userName.textContent = popupProfileNameInput.value;
+	userJob.textContent = popupProfileJobInput.value;
+  
+	closePopup(popupProfile);
+  
+	profileForm.reset();
+}
+profileForm.addEventListener('submit', formSubmitHandler);
+
+// Функция добавления новой фотокарточки
+function handleNewCardSubmit(evt) {
+	evt.preventDefault();
+
+	elementList.prepend(createCard(popupProfileLinkInput.value, popupProfileTitleInput.value));
+
+	closePopup(popupAddPhoto);
+	photoForm.reset();
+}
+photoForm.addEventListener('submit', handleNewCardSubmit);
 
